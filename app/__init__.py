fe48f76170c288ffacd7e9e22f298ac1d6bbdaa8
smartteamscorp <167222235+smartteamscorp@users.noreply.github.com>
2024-04-18 13:56:04 +0100

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@
 from flask import Flask
 from config import SECRET_KEY
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 app.config['SECRET_KEY'] = SECRET_KEY
 
 from app import views  # Se as rotas estão definidas em views.py
